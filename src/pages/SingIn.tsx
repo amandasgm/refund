280430c556aaa60328c71a8c0d7316ef163ecd8d
Amanda <amanda.asgm@gmail.com>
2026-02-28 18:16:11 -0300

    feat(signIn): setando email e password

--- src/pages/SingIn.tsx
+++ src/pages/SingIn.tsx
@@ -1,18 +1,36 @@
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
+import { useState } from "react";
+
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSend(e: React.SyntheticEvent) {
+    e.preventDefault();
+    console.log(email, password)
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form onSubmit={onSend} className="w-full flex flex-col gap-4">
       <Input
         legend="email"
         required
         type="email"
         placeholder="seuemail@gmail.com"
+        onChange={(e) => setEmail(e.target.value)}
       />
-      <Input legend="senha" required type="password" placeholder="********" />
+      <Input
+        legend="senha"
+        required
+        type="password"
+        placeholder="********"
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
-      <Button children="Entrar" />
+      <Button type="submit" children="Entrar" isLoading={isLoading}/>
       <Button children="Criar conta" bg="without" />
     </form>
   );
