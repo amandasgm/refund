a0663635c89fdc1bba1a732d737252116fad7bdb
Amanda <amanda.asgm@gmail.com>
2026-03-04 20:16:43 -0300

    feat(dashboar): objeto exemplo

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,9 +1,23 @@
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
+import { RefundItem } from "../components/RefundItem";
 
-import lupaSvg from "../assets/search.svg"
+import lupaSvg from "../assets/search.svg";
+import { CATEGORIES } from "../utils/categories";
 
 import { useState } from "react";
+
+const REFUND_EXEMPLE = [
+  {
+    id: "123",
+    name: "Amanda",
+    category: "Hospedagem",
+    amount: "R$524,90",
+    categoryImage: CATEGORIES["accommodation"].icon,
+  },
+  
+
+];
 
 export function DashBoard() {
   const [name, setName] = useState("");
@@ -29,6 +43,11 @@ export function DashBoard() {
           <img src={lupaSvg} alt="" />
         </Button>
       </form>
+      <div className="flex flex-col gap-4 my-4 max-h-[342px] overflow-y-scroll">
+        {REFUND_EXEMPLE.map((item) => (
+          <RefundItem key={item.id} data={item} />
+        ))}
+      </div>
     </div>
   );
 }
