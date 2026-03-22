8e813a9ecb5d909e2663ff49e40cf88b271d6f86
Amanda <amanda.asgm@gmail.com>
2026-03-22 14:27:29 -0300

    chore(context): tipando contexto

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
 import { createContext, type ReactNode } from "react";
+
+// tipagem da sessao
+type AuthContext = {
+  session: null | UserAPIResponse
+}
 
 export const AuthContext = createContext({});
 
+// provider do context
 export function AuthProvider({ children }: { children: ReactNode }) {
   return (
     <AuthContext.Provider value={{ name: "Amanda Santana" }}>
