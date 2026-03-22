7f09ece2339dd4363c4b80f5d7fd772826315fe6
Amanda <amanda.asgm@gmail.com>
2026-03-21 23:53:20 -0300

    feat: recuperando dados do banco e fazendo autorizacao

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,7 +1,8 @@
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
-
+import { api } from "../services/api"
+import { AxiosError } from "axios"
 
 import { z, ZodError } from "zod";
 
@@ -23,10 +24,16 @@ export function SignIn() {
         password: formData.get("password")
       })
   
-      console.log(data)
+      const response = await api.post("/sessions", data)
+      console.log(response.data)
+
     } catch (error) {
       if(error instanceof ZodError){
         return {message: error.issues[0].message}
+      }
+
+      if(error instanceof AxiosError){
+        return {message: error.response?.data.message}
       }
       return {message: "Não foi possivel acessar"}
     }
