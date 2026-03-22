e1db3a713a2a06c883ef05434b434e48cc70e2dc
Amanda <amanda.asgm@gmail.com>
2026-03-21 21:40:18 -0300

    feat(signIn): validando e recuperando dados validados + mensagem de erro

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -1,18 +1,37 @@
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 
+
+
+import { z, ZodError } from "zod";
+
 import { useActionState } from "react"; // para lidar com o estado de uma acao
 
+const signInSchema = z.object({
+  email: z.string().email({ message: "Email invalido"}),
+  password: z.string().trim().min(1, {message: "Informe a senha"})
+})
+
 export function SignIn() {
-  const [state, formAction, isLoading] = useActionState(onSend, {
-    email: "amanda@gmail.com",
-    password: "123",
-  });
+  const [state, formAction, isLoading] = useActionState(onSend, null)
 
   // quando clicar em enviar
-  function onSend(prevState: any, formData: FormData) {
-    console.log(formData.get("email"));
-  }
+  async function onSend(_: any, formData: FormData) {
+    try {
+      const data = signInSchema.parse({
+        email: formData.get("email"),
+        password: formData.get("password")
+      })
+  
+      console.log(data)
+    } catch (error) {
+      if(error instanceof ZodError){
+        return {message: error.issues[0].message}
+      }
+      return {message: "Não foi possivel acessar"}
+    }
+    // tudo que é retornado aqui, fica dentro do state
+  } 
 
   // html
   return (
@@ -23,7 +42,6 @@ export function SignIn() {
         required
         type="email"
         placeholder="seuemail@gmail.com"
-        defaultValue={state.email}
       />
       <Input
         name="password"
@@ -31,7 +49,10 @@ export function SignIn() {
         required
         type="password"
         placeholder="********"
-      />
+      /> 
+      <p className="text-sm text-red-600 text-center my-4 font-medium">
+        {state?.message}
+      </p>
 
       <Button type="submit" children="Entrar" isLoading={isLoading} />
       <a
